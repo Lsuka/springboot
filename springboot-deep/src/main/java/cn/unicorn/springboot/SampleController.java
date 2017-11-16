dec0c9a3be6dad1187adb18958a7391ccbbb7a47
Lsuka <1150859699@qq.com>
2017-11-16 20:21:41 +0800

    上传测试

---
+++ springboot-deep/src/main/java/cn/unicorn/springboot/SampleController.java
@@ -0,0 +1,14 @@
+package cn.unicorn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@SpringBootApplication
+public class SampleController {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleController.class, args);
+	}
+}
